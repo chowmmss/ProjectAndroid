b817c540a1c365bb4f94ed89595e8f6f9f39ad4a
zhoummss <chowms47@163.com>
2018-09-17 22:20:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zhou/myfirst/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhou.myfirst;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
